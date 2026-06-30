bc6b43cb1c61d22232bc609fb468a69ab4ca5bd4
vishalyadav854581 <vishalyadav854581@gmail.com>
2026-06-30 06:10:24 +0000

    Added DOM selectors examples

---
+++ .devcontainer/01_Basics/01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 122533
+let accountEmail = "Vishal@7gmail.com"
+var accountpassword = "12345"
+accountcity = "Jaipur"
+
+//accountId = 2 
+
+let accountEmail = "Vishal&@gmail.com"
+var accountpassword = "8545"
+accountcity = "Jaunpur"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountpassword, accountcity])
